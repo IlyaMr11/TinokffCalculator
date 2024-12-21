94b8b21d41385bc3addb017c03a66bcd590c4766
ilyaMr <ilya112006@yandex.ru>
2024-12-21 22:37:03 +0300

    addApp

---
+++ TinkoffCalculator/TinkoffCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Илья Морозов on 21.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
